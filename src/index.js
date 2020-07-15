f635d2690b1aa9a837a48a6c6df510dc3a7a312c
Alex Doss <alex.doss42@gmail.com>
2020-07-14 22:10:24 -0600

    cleans house

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div>
+    My new content
+  </div>,
   document.getElementById('root')
 );
 
