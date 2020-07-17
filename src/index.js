227f257b40a83ae267057b55319280a32c6f6014
Alex Doss <alex.doss42@gmail.com>
2020-07-16 22:17:59 -0600

    add an increment button

--- src/index.js
+++ src/index.js
@@ -5,9 +5,14 @@ import * as serviceWorker from './serviceWorker';
 const App = () => {
   const [count, setCount] = useState(0);
 
+  const increment = () => {
+    setCount(count + 1);
+  };
+
   return (
     <div>
       <p>The current count is {count}</p>
+      <button onClick={increment}>+1</button>
     </div>
   )
 };
