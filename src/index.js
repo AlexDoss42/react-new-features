4392b2a12d10608f16f712b7f8959e95aa85d70d
Alex Doss <alex.doss42@gmail.com>
2020-07-16 22:43:08 -0600

    adds Notes app function

--- src/index.js
+++ src/index.js
@@ -2,36 +2,62 @@ import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
-const App = () => {
-  const [count, setCount] = useState(0);
-  const [text, setText] = useState('');
+const NoteApp = () => {
+  const [notes, setNotes] = useState([]);
+  const [title, setTitle] = useState('');
 
-  const increment = () => {
-    setCount(count + 1);
-  };
-
-  const decrement = () => {
-    setCount(count - 1);
-  };
-
-  const reset = () => {
-    setCount(0);
+  const addNote = (e) => {
+    e.preventDefault();
+    setNotes([
+      ...notes,
+      {
+        title
+      }
+    ]);
   };
 
   return (
     <div>
-      <p>The current {text || 'count'} is {count}</p>
-      <button onClick={increment}>+1</button>
-      <button onClick={decrement}>-1</button>
-      <button onClick={reset}>Reset</button>
-      <input value={text} onChange={(e) => {setText(e.target.value)}} />
+      <h1>Notes</h1>
+      <p>Add note</p>
+      <form onSubmit={addNote}>
+        <input value={title} onChange={(e) => setTitle(e.target.value)} />
+        <button>add note</button>
+      </form>
     </div>
-  )
+  );
 };
+
+// const App = () => {
+//   const [count, setCount] = useState(0);
+//   const [text, setText] = useState('');
+
+//   const increment = () => {
+//     setCount(count + 1);
+//   };
+
+//   const decrement = () => {
+//     setCount(count - 1);
+//   };
+
+//   const reset = () => {
+//     setCount(0);
+//   };
+
+//   return (
+//     <div>
+//       <p>The current {text || 'count'} is {count}</p>
+//       <button onClick={increment}>+1</button>
+//       <button onClick={decrement}>-1</button>
+//       <button onClick={reset}>Reset</button>
+//       <input value={text} onChange={(e) => {setText(e.target.value)}} />
+//     </div>
+//   )
+// };
 
 
 ReactDOM.render(
-  <App/>,
+  <NoteApp/>,
   document.getElementById('root')
 );
 
