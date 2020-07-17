238ee6faf9d293356ef55c3f08f70b6bce414416
Alex Doss <alex.doss42@gmail.com>
2020-07-16 22:23:02 -0600

    adds decrement and reset button

--- src/index.js
+++ src/index.js
@@ -9,10 +9,19 @@ const App = () => {
     setCount(count + 1);
   };
 
+  const decrement = () => {
+    setCount(count - 1);
+  };
+  const reset = () => {
+    setCount(0);
+  };
+
   return (
     <div>
       <p>The current count is {count}</p>
       <button onClick={increment}>+1</button>
+      <button onClick={decrement}>-1</button>
+      <button onClick={reset}>Reset</button>
     </div>
   )
 };
