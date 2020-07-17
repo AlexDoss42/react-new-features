b5f6f064015e01c3742d58776b8f740b5b9ec1f1
Alex Doss <alex.doss42@gmail.com>
2020-07-17 17:52:19 -0600

    adds a use effect to change the tab title to the count

--- src/index.js
+++ src/index.js
@@ -1,79 +1,84 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
-const NoteApp = () => {
-  const [notes, setNotes] = useState([]);
-  const [title, setTitle] = useState('');
-  const [body, setBody] = useState('');
+// const NoteApp = () => {
+//   const [notes, setNotes] = useState([]);
+//   const [title, setTitle] = useState('');
+//   const [body, setBody] = useState('');
 
-  const addNote = (e) => {
-    e.preventDefault();
-    setNotes([
-      ...notes,
-      {
-        title,
-        body
-      }
-    ]);
-  };
-
-  const removeNote = (title) => {
-    setNotes(notes.filter((note) => note.title !== title));
-  };
-
-  return (
-    <div>
-      <h1>Notes</h1>
-      {
-        notes.map((note) => (
-          <div key={note.title}>
-            <h3>{note.title}</h3>
-            <p>{note.body}</p>
-            <button onClick={() => removeNote(note.title)}>x</button>
-          </div>
-        ))
-      }
-      <p>Add note</p>
-      <form onSubmit={addNote}>
-        <input value={title} onChange={(e) => setTitle(e.target.value)} />
-        <textarea value={body} onChange={(e) => setBody(e.target.value)}></textarea>
-        <button>add note</button>
-      </form>
-    </div>
-  );
-};
-
-// const App = () => {
-//   const [count, setCount] = useState(0);
-//   const [text, setText] = useState('');
-
-//   const increment = () => {
-//     setCount(count + 1);
+//   const addNote = (e) => {
+//     e.preventDefault();
+//     setNotes([
+//       ...notes,
+//       {
+//         title,
+//         body
+//       }
+//     ]);
 //   };
 
-//   const decrement = () => {
-//     setCount(count - 1);
-//   };
-
-//   const reset = () => {
-//     setCount(0);
+//   const removeNote = (title) => {
+//     setNotes(notes.filter((note) => note.title !== title));
 //   };
 
 //   return (
 //     <div>
-//       <p>The current {text || 'count'} is {count}</p>
-//       <button onClick={increment}>+1</button>
-//       <button onClick={decrement}>-1</button>
-//       <button onClick={reset}>Reset</button>
-//       <input value={text} onChange={(e) => {setText(e.target.value)}} />
+//       <h1>Notes</h1>
+//       {
+//         notes.map((note) => (
+//           <div key={note.title}>
+//             <h3>{note.title}</h3>
+//             <p>{note.body}</p>
+//             <button onClick={() => removeNote(note.title)}>x</button>
+//           </div>
+//         ))
+//       }
+//       <p>Add note</p>
+//       <form onSubmit={addNote}>
+//         <input value={title} onChange={(e) => setTitle(e.target.value)} />
+//         <textarea value={body} onChange={(e) => setBody(e.target.value)}></textarea>
+//         <button>add note</button>
+//       </form>
 //     </div>
-//   )
+//   );
 // };
+
+const App = (props) => {
+  const [count, setCount] = useState(props.count);
+  const [text, setText] = useState('');
+
+  useEffect(() => {
+    console.log('useEffect ran');
+    document.title = count;
+  })
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  const reset = () => {
+    setCount(0);
+  };
+
+  return (
+    <div>
+      <p>The current {text || 'count'} is {count}</p>
+      <button onClick={increment}>+1</button>
+      <button onClick={decrement}>-1</button>
+      <button onClick={reset}>Reset</button>
+      <input value={text} onChange={(e) => {setText(e.target.value)}} />
+    </div>
+  )
+};
 
 
 ReactDOM.render(
-  <NoteApp/>,
+  <App count={0}/>,
   document.getElementById('root')
 );
 
